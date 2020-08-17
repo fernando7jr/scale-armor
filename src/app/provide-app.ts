565c9786b8a8881e24dc519f89f4dd49af087a30
FRSJR <fernando@altbank.co>
2020-08-17 20:38:04 -0300

    feat: improve typing

--- src/app/provide-app.ts
+++ src/app/provide-app.ts
@@ -2,16 +2,18 @@ import 'reflect-metadata';
 import { SimpleAppProvider } from './simple-app';
 import { AppProvider } from './app';
 
+export type ClassConstructor = Function | Object;
+
 export function ProvideApp(name: string): ClassDecorator {
-    return (target: Object) => {
+    return (target: ClassConstructor) => {
         ProvideApp.defineAppProvider(name, target);
     };
 };
 
-ProvideApp.defineAppProvider = function (name: string, target: Object,): void {
+ProvideApp.defineAppProvider = function (name: string, target: ClassConstructor): void {
     Reflect.defineMetadata('appProviver', new SimpleAppProvider(name), target);
 };
 
-ProvideApp.getAppProvider = function (target: Object): AppProvider | undefined {
+ProvideApp.getAppProvider = function (target: ClassConstructor): AppProvider | undefined {
     return Reflect.getMetadata('appProviver', target);
 };
