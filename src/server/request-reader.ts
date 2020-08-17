5e495ebfc76218af3108f24b88d42edb2c83f39a
FRSJR <fernando@altbank.co>
2020-08-17 10:25:01 -0300

    fix: add fallback for undefined params or headers

--- src/server/request-reader.ts
+++ src/server/request-reader.ts
@@ -4,7 +4,7 @@ import { RequestReader, Request, Params, RequestHead, RequestBody } from '../app
 type Callback<T> = (t: T) => void;
 export type BeforeHook = (request: Request) => Request;
 export abstract class RequestReaderBase implements RequestReader {
-    constructor(private before?: BeforeHook) {}
+    constructor(private before?: BeforeHook) { }
 
     abstract get head(): RequestHead;
     abstract get hasBody(): boolean;
@@ -13,7 +13,7 @@ export abstract class RequestReaderBase implements RequestReader {
     protected abstract onDataChunk(onChunk: (chunk: Buffer) => void): Promise<{}>;
     protected abstract readBodyBuffer(): Promise<Buffer>;
 
-    private getBodyParser<T=unknown>(contentType: String, encoding?: BufferEncoding): (data: Buffer) => RequestBody<T> {
+    private getBodyParser<T = unknown>(contentType: String, encoding?: BufferEncoding): (data: Buffer) => RequestBody<T> {
         switch (contentType) {
             case 'application/json':
                 return (data: Buffer) => {
@@ -50,7 +50,7 @@ export abstract class RequestReaderBase implements RequestReader {
         };
     }
 
-    protected async readBody<T=unknown>(): Promise<RequestBody<T>> {
+    protected async readBody<T = unknown>(): Promise<RequestBody<T>> {
         const head = this.head;
         const contentType = head.contentType;
         const contentLength = head.contentLength;
@@ -63,14 +63,14 @@ export abstract class RequestReaderBase implements RequestReader {
 
         return parser(data);
     }
-    
-    async read<T=unknown>(): Promise<Request<T>> {
+
+    async read<T = unknown>(): Promise<Request<T>> {
         const requestHead = this.head;
         const requestBody = await this.readBody();
         const request: Request = {
-            headers: requestHead.headers,
+            headers: requestHead.headers || {},
             method: requestHead.method,
-            params: requestHead.params,
+            params: requestHead.params || {},
             path: requestHead.path,
             route: requestHead.route,
             appName: requestHead.appName,
@@ -89,8 +89,8 @@ export abstract class RequestReaderBase implements RequestReader {
         return request;
     }
 
-    stream(encoding: string, onChunk: (chunk: string) => void): Promise<{}>
-    stream(onChunk: (chunk: Buffer) => void): Promise<{}>
+    stream(encoding: string, onChunk: (chunk: string) => void): Promise<{}>;
+    stream(onChunk: (chunk: Buffer) => void): Promise<{}>;
     stream(encoding: string | Callback<Buffer>, onChunk?: Callback<string>): Promise<{}> {
         if (encoding instanceof Function) {
             return this.onDataChunk(encoding);
