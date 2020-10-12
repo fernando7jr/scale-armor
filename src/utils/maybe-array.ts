3dca2cc0b4bc1a9334e8f9a46abc179d0139d763
FRSJR <fernando@altbank.co>
2020-10-12 14:55:45 -0300

    doc(utils): document utils module

--- src/utils/maybe-array.ts
+++ src/utils/maybe-array.ts
@@ -1,1 +1,2 @@
+/** Defines a type which can be an array of T or T itself */
 export type MaybeArray<T> = T | T[];
--- src/utils/paging.ts
+++ src/utils/paging.ts
@@ -2,16 +2,33 @@
  * Interface for pagination options
  */
 export interface PaginationOptions {
+    /** The desired page. Starts at 1 */
     page: number;
+    /** The size of each page */
     pageSize: number;
+    /** The property name to use for sorting the data */
     sortBy: string;
+    /**
+     * The type of sorting
+     * 1 for Ascending
+     * -1 for Descending
+     */
     sortType: 1 | -1;
 }
 
+/**
+ * Interface for PaginatedData
+ * @template T - the data type
+ */
 export interface PaginatedData<T> {
+    /** The data array */
     data: T[];
+    /** The requested page */
     page: number;
+    /** The size of each page */
     pageSize: number;
+    /** The last page which can be fetched */
     lastPage: number;
+    /** The total length of the available data, counting from the first page until the last one */
     total: number;
 }
